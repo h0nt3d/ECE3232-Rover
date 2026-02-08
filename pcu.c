9ff323a428776e3ad8469292b940df9f18e23e6b
h0nt3d <akanoim@gmail.com>
2026-02-07 21:36:37 -0400

    Added get flsySky info

--- pcu.c
+++ pcu.c
@@ -2,6 +2,7 @@
 #include "init.h"
 
 volatile uint8_t get_pcu_info[6] = {0xFE, 0x19, 0x01, 0x04, 0x00, 0x00};
+volatile uint8_t get_flySky_info[6] = {0xFE, 0x19, 0x01, 0x05, 0x00, 0x00};
 
 void send_get_pcu_info()
 {
@@ -11,11 +12,19 @@ void send_get_pcu_info()
     }
 }
 
+void send_get_flySky_info()
+{
+    for (int i = 0; i < 6; i++) {
+        TX1REG = get_flySky_info[i];
+        while (!TX1STAbits.TRMT) {} // wait until register is empty
+    }
+}
 
 volatile uint8_t rxCount = 0;
 volatile uint8_t rxDone = 0;
 
 volatile uint8_t get_pcu_info_buf[12];
+volatile uint8_t get_flySky_info_buf[26];
 
 
 void __interrupt() ISR() 
@@ -25,24 +34,22 @@ void __interrupt() ISR()
         rxCount = 0;
         rxDone = 0;
         
-        send_get_pcu_info();
+        //send_get_pcu_info();
+        send_get_flySky_info();
         IOCAFbits.IOCAF5 = 0;  // Clear IOC flag
     } 
     
-    if (PIE3bits.RCIE && PIR3bits.RCIF)
-    {
+    if (PIE3bits.RCIE && PIR3bits.RCIF) {
         // handle overrun
-        if (RC1STAbits.OERR)
-        {
+        if (RC1STAbits.OERR) {
             RC1STAbits.CREN = 0;
             RC1STAbits.CREN = 1;
         }
 
         uint8_t b = RC1REG;    // reading clears RCIF
 
-        if (!rxDone && rxCount < 12)
-        {
-            get_pcu_info[rxCount++] = b;
+        if (!rxDone && rxCount < 12) {
+            get_flySky_info_buf[rxCount++] = b;
             if (rxCount == 12) rxDone = 1;
         }
     }
